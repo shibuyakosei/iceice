d7d72b45b2c19f7ed7a5b4b5b1733547abfe3707
kifr-egrant <kenta.kihara@e-grant.net>
2022-04-17 20:04:58 +0900

    add template

---
+++ src/components/templates/TwoColums.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="page_root">
+    <div class="top">
+      <slot name="top" />
+    </div>
+
+    <div class="left">
+      <slot name="left" />
+    </div>
+
+    <div class="right">
+      <slot name="right" />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.page_root {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.top {
+  width: 100%;
+}
+
+.left {
+  width: 40%;
+  padding: 20px;
+}
+
+.right {
+  width: 60%;
+  padding: 20px;
+}
+
+
+</style>
